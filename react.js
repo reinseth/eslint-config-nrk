5aee9b0c01e5bd72db9e914a33aefe2c1d75fad5
Jan-Erik Bråthen <Jan-Erik.Brathen@nrk.no>
2015-10-27 15:30:12 +0100

    Add prefer-es6-class to react rules

--- react.js
+++ react.js
@@ -39,6 +39,7 @@ module.exports = {
     "react/no-multi-comp": 2,                 // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
     "react/no-set-state": 0,                  // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-set-state.md
     "react/no-unknown-property": 2,           // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+    "react/prefer-es6-class": 0,              // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-es6-class.md
     "react/prop-types": 2,                    // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
     "react/react-in-jsx-scope": 2,            // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
     "react/require-extension": 0,             // github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
